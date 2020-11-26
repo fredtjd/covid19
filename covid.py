cb05e9908f36944a6bb001de95d9bdb3b2341a3a
Fred <6234265-fredtjd@users.noreply.gitlab.com>
2020-11-26 19:32:02 +0000

    Updated for multiple processing

--- covid.py
+++ covid.py
@@ -1,35 +1,29 @@
 import pandas as pd
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
-from git import Repo
 import os
 
 python_dir = os.path.dirname(os.path.realpath(__file__))
 
-latestData = 'https://api.coronavirus.data.gov.uk/v2/data?areaType=overview&metric=newAdmissions&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv'
+UK = ['UK', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=overview&metric=newAdmissions&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv']
+London = ['London', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=region&areaCode=E12000007&metric=newAdmissions&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv']
+
 endDate = pd.to_datetime(datetime.today() - timedelta(days=2))
 
-df = pd.read_csv(latestData, index_col=0)
-df = df.drop(['areaType', 'areaCode', 'areaName'], axis = 1)
-df.index = pd.to_datetime(df.index)
-df = df.sort_index()
+adm = ['newAdmissions', 'New Admissions']
+pub = ['newCasesByPublishDate', 'New Cases by Publication Date']
+spec = ['newCasesBySpecimenDate', 'New Cases by Specimen Date']      
 
-df['newAdmissions7dayRollingAvg'] = df['newAdmissions'].rolling(window=7).mean()
-df['newCasesByPublishDate7dayRollingAvg'] = df['newCasesByPublishDate'].rolling(window=7).mean()
-df['newCasesBySpecimenDate7dayRollingAvg'] = df['newCasesBySpecimenDate'].rolling(window=7).mean()
+def dataManipulator(location):
+    df = pd.read_csv(location[1], index_col=0)
+    df = df.drop(['areaType', 'areaCode', 'areaName'], axis = 1)
+    df.index = pd.to_datetime(df.index)
+    df = df.sort_index()
+    df = df['2020-03-01':endDate]
+    for dt in (adm, pub, spec):
+        df[dt[0] + '7dayRollingAvg'] = df[dt[0]].rolling(window=7).mean()
+        df.plot(y=[dt[0], dt[0] + '7dayRollingAvg'], ylim=0, title=dt[1])
+        plt.savefig(location[0] + ' ' + dt[0] + '.png')
 
-df = df['2020-03-01':endDate]
-
-repo = Repo(os.path.join(python_dir, '.git'))
-
-def grapher(graphData, graphTitle):
-    df.plot(y=[graphData, graphData + '7dayRollingAvg'], ylim=0, title=graphTitle)
-    plt.savefig(graphData + '.png')
-    repo.git.add(graphData + '.png')
-
-grapher('newAdmissions', 'New Admissions')
-grapher('newCasesByPublishDate', 'New Cases by Publication Date')
-grapher('newCasesBySpecimenDate', 'New Cases by Specimen Date')
-
-repo.index.commit('Updated graphs')
-repo.remote(name='origin').push()
+dataManipulator(London)
+dataManipulator(UK)
