c5db6cec7ca9acfcfe03516df58f247ff9a004de
Fred <6234265-fredtjd@users.noreply.gitlab.com>
2020-12-14 21:37:03 +0000

    Updated again

--- covid.py
+++ covid.py
@@ -10,21 +10,29 @@ python_dir = os.path.dirname(os.path.realpath(__file__))
 
 UK = ['UK', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=overview&metric=newAdmissions&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 68.0]
 London = ['London', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=region&areaCode=E12000007&metric=newAdmissions&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 93.0]
+Liverpool = ['Liverpool', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&areaCode=E08000012&metric=newAdmissions&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 4.9]
+Manchester = ['Manchester', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&areaCode=E08000003&metric=newAdmissions&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 5.4]
+Birmingham = ['Birmingham', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&areaCode=E08000025&metric=newAdmissions&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 13.0]
 Scotland = ['Scotland', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=S92000003&metric=newAdmissions&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 54.7]
 Wales = ['Wales', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=W92000004&metric=newAdmissions&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 32.0]
 
-endDate = pd.to_datetime(datetime.today() - timedelta(days=1))
-startDate = '2020-08-01'
+#endDate = pd.to_datetime(datetime.today() - timedelta(days=1))
+#startDate = '2020-08-01'
 
 adm = ['newAdmissions', 'New Admissions']
 pub = ['newCasesByPublishDate', 'New Cases by Publication Date']
 spec = ['newCasesBySpecimenDate', 'New Cases by Specimen Date']
 
 def dataManipulator(plot_type):
+    startDate = '2020-08-01'
+    if plot_type == spec:
+        endDate = pd.to_datetime(datetime.today() - timedelta(days=5))
+    else:
+        endDate = pd.to_datetime(datetime.today() - timedelta(days=1))
     plt.title(plot_type[1])
-    fig, axes = plt.subplots(nrows=3, ncols=1)
+    fig, axes = plt.subplots(nrows=5, ncols=1)
     counter = 0
-    for location in (London, Scotland, Wales):
+    for location in (London, Liverpool, Manchester, Scotland, Wales):
         df = pd.read_csv(location[1], index_col=0)
         df = df.drop(['areaType', 'areaCode', 'areaName'], axis = 1)
         df.index = pd.to_datetime(df.index)
@@ -33,8 +41,9 @@ def dataManipulator(plot_type):
 #    for dt in (adm, pub, spec):
         #df[plot_type[0] + '7dayRollingAvg'] = df[plot_type[0]].rolling(window=7).mean()
         df[plot_type[0] + 'per100k'] = df[plot_type[0]].div(location[2])
-        #df.plot.area(figsize=(12,4),  y=[dt[0], dt[0] + '7dayRollingAvg'], ylim=0, title=location[0] + ' ' + dt[1])
-        df.plot.area(figsize=(10,8), ax=axes[counter], y=plot_type[0] + 'per100k', ylim=0, title=location[0], legend=None)
+        df[plot_type[0] + 'avg'] = df[plot_type[0]].rolling(window=7).mean()
+        df[plot_type[0] + 'per100kavg'] = df[plot_type[0] + 'per100k'].rolling(window=7).mean()
+        df.plot.area(figsize=(8,10), ax=axes[counter], y=plot_type[0] + 'per100kavg', ylim=(0, 100), title=location[0], legend=None)
         axes[counter].set_xlabel('')
         counter += 1
     fig.tight_layout()
@@ -44,4 +53,4 @@ def dataManipulator(plot_type):
 
 dataManipulator(spec)
 dataManipulator(pub)
-dataManipulator(adm)
+#dataManipulator(adm)
