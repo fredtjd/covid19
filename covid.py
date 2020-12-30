744aecc0281c43651a078613163eb6daeb9f1a1f
Fred <6234265-fredtjd@users.noreply.gitlab.com>
2020-12-30 13:09:01 +0000

    Update processing and add new areas

--- covid.py
+++ covid.py
@@ -11,13 +11,16 @@ London = ['London', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=region
 Scotland = ['Scotland', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=S92000003&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 54.7]
 Wales = ['Wales', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=W92000004&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 32.0]
 NI = ['NI', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=N92000002&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 18.97]
+Oxon = ['Oxfordshire', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&areaCode=E10000025&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 6.91] #http://insight.oxfordshire.gov.uk/cms/population
 
 pub = ['newCasesByPublishDate', 'New Cases by Publication Date']
 spec = ['newCasesBySpecimenDate', 'New Cases by Specimen Date']
 
+locs_to_parse = [London, Scotland, Wales, NI, Oxon]
+
 def dataFrame():
     df = {}
-    for location in (London, Scotland, NI, Wales):
+    for location in locs_to_parse:
         df[location[0]] = pd.read_csv(location[1], index_col=0)
         df[location[0]] = df[location[0]].drop(['areaType', 'areaCode', 'areaName'], axis = 1)
         df[location[0]].index = pd.to_datetime(df[location[0]].index)
@@ -41,7 +44,7 @@ def dataPlotter(plot_type, look_back, percapita, avg):
         multiplot_details = [plot_type[0] + 'avg', plot_type[1] + ' (7 day average)']
     else:
         multiplot_details = [plot_type[0], plot_type[1]]
-    for location in (London, Scotland, Wales, NI):
+    for location in locs_to_parse:
         df[location[0]] = df[location[0]][startDate:endDate]
         plt.plot(df[location[0]][multiplot_details[0]], label=location[0])
     plt.title(multiplot_details[1])
@@ -56,6 +59,9 @@ def dataPlotter(plot_type, look_back, percapita, avg):
 
 if __name__ == '__main__':
     dataPlotter(spec, 5, True, True) #per100k/avg
-    dataPlotter(pub, 1, True, True) #per100k/avg
+    dataPlotter(pub, 0, True, True) #per100k/avg
     dataPlotter(spec, 5, True, False) #per100k
-    dataPlotter(pub, 1, True, False) #per100k
+    dataPlotter(pub, 0, True, False) #per100k
+    dataPlotter(pub, 0, False, False)
+    dataPlotter(pub, 0, False, True)
+    dataPlotter(spec, 5, False, True)
