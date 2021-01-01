c4caae8e126ff3e56183ecd5797935c9a9937e43
Fred <6234265-fredtjd@users.noreply.gitlab.com>
2021-01-01 15:14:46 +0000

    Updated a lot

--- covid.py
+++ covid.py
@@ -2,33 +2,61 @@ import pandas as pd
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 from matplotlib.dates import MonthLocator, DateFormatter
-import os
+import os, sys, time
+import urllib.request as req
 
 python_dir = os.path.dirname(os.path.realpath(__file__))
+csv_dir = os.path.join(python_dir, 'csv')
 
 #population values used from https://www.ukpopulation.org
-London = ['London', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=region&areaCode=E12000007&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 89.5]
-Scotland = ['Scotland', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=S92000003&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 54.7]
-Wales = ['Wales', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=W92000004&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 32.0]
-NI = ['NI', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=N92000002&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 18.97]
-Oxon = ['Oxfordshire', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&areaCode=E10000025&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&format=csv', 6.91] #http://insight.oxfordshire.gov.uk/cms/population
+London = ['London', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=region&areaCode=E12000007&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&metric=newDeathsByPublishDate&format=csv', 89.5]
+Scotland = ['Scotland', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=S92000003&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&metric=newDeathsByPublishDate&format=csv', 54.7]
+Wales = ['Wales', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=W92000004&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&metric=newDeathsByPublishDate&format=csv', 32.0]
+NI = ['NI', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=N92000002&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&metric=newDeathsByPublishDate&format=csv', 18.97]
+Oxon = ['Oxfordshire', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&areaCode=E10000025&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&metric=newDeathsByPublishDate&format=csv', 6.91] #http://insight.oxfordshire.gov.uk/cms/population
 
-pub = ['newCasesByPublishDate', 'New Cases by Publication Date']
+pub = ['newCasesByPublishDate', 'New Cases by Publish Date']
 spec = ['newCasesBySpecimenDate', 'New Cases by Specimen Date']
+deaths = ['newDeathsByPublishDate', 'Deaths by Publish Date']
 
-locs_to_parse = [London, Scotland, Wales, NI, Oxon]
+locations = [London, Scotland, Wales, NI, Oxon]
+
+def csvDownloader():
+    #Checks if today's date is in currently downloaded csv, if not checks 1 on the server and only downloads all csvs if they contain today's date and exits the script if nothing new is downloaded
+    if not os.path.isfile(os.path.join(csv_dir, London[0] + '.csv')):
+        for loc in locations:
+            req.urlretrieve(loc[1], os.path.join(csv_dir, loc[0] + '.csv'))
+    else:
+        da = str(datetime.today().date())
+        print('Checking downloaded csv.')
+        df = pd.read_csv(os.path.join(csv_dir, London[0] + '.csv'), index_col=0)
+        df.index = pd.to_datetime(df.index)
+        df_test = df.loc[df.index == da]
+        if df_test.empty:
+            print('Checking server csv.')
+            df = pd.read_csv(London[1], index_col=0)
+            df.index = pd.to_datetime(df.index)
+            df_test = df.loc[df.index == da]
+            if not df_test.empty:
+                print('Downloading from server.')
+                for loc in locations:
+                    req.urlretrieve(loc[1], os.path.join(csv_dir, loc[0] + '.csv'))
+            else:
+                print('Now new updates. Exiting.')
+                sys.exit()
 
 def dataFrame():
     df = {}
-    for location in locs_to_parse:
-        df[location[0]] = pd.read_csv(location[1], index_col=0)
-        df[location[0]] = df[location[0]].drop(['areaType', 'areaCode', 'areaName'], axis = 1)
-        df[location[0]].index = pd.to_datetime(df[location[0]].index)
-        df[location[0]] = df[location[0]].sort_index()
+    for loc in locations:
+        csv_file = os.path.join(csv_dir, loc[0] + '.csv')
+        df[loc[0]] = pd.read_csv(csv_file, index_col=0)
+        df[loc[0]] = df[loc[0]].drop(['areaType', 'areaCode', 'areaName'], axis = 1)
+        df[loc[0]].index = pd.to_datetime(df[loc[0]].index)
+        df[loc[0]] = df[loc[0]].sort_index()
         for plot_type in (pub, spec):
-            df[location[0]][plot_type[0] + 'per100k'] = df[location[0]][plot_type[0]].div(location[2])
-            df[location[0]][plot_type[0] + 'avg'] = df[location[0]][plot_type[0]].rolling(window=7).mean()
-            df[location[0]][plot_type[0] + 'per100kavg'] = df[location[0]][plot_type[0] + 'per100k'].rolling(window=7).mean()
+            df[loc[0]][plot_type[0] + 'per100k'] = df[loc[0]][plot_type[0]].div(loc[2])
+            df[loc[0]][plot_type[0] + 'avg'] = df[loc[0]][plot_type[0]].rolling(window=7).mean()
+            df[loc[0]][plot_type[0] + 'per100kavg'] = df[loc[0]][plot_type[0] + 'per100k'].rolling(window=7).mean()
     return df
 
 def dataPlotter(plot_type, look_back, percapita, avg):
@@ -44,9 +72,9 @@ def dataPlotter(plot_type, look_back, percapita, avg):
         multiplot_details = [plot_type[0] + 'avg', plot_type[1] + ' (7 day average)']
     else:
         multiplot_details = [plot_type[0], plot_type[1]]
-    for location in locs_to_parse:
-        df[location[0]] = df[location[0]][startDate:endDate]
-        plt.plot(df[location[0]][multiplot_details[0]], label=location[0])
+    for loc in locations:
+        df[loc[0]] = df[loc[0]][startDate:endDate]
+        plt.plot(df[loc[0]][multiplot_details[0]], label=loc[0])
     plt.title(multiplot_details[1])
     plt.ylim(0)
     plt.legend()
@@ -58,10 +86,13 @@ def dataPlotter(plot_type, look_back, percapita, avg):
     plt.savefig(os.path.join(python_dir, multiplot_details[1] + '.png'))
 
 if __name__ == '__main__':
-    dataPlotter(spec, 5, True, True) #per100k/avg
-    dataPlotter(pub, 0, True, True) #per100k/avg
-    dataPlotter(spec, 5, True, False) #per100k
-    dataPlotter(pub, 0, True, False) #per100k
-    dataPlotter(pub, 0, False, False)
+    csvDownloader()
+    dataPlotter(pub, 0, True, True)
+    dataPlotter(pub, 0, True, False)
     dataPlotter(pub, 0, False, True)
-    dataPlotter(spec, 5, False, True)
+    dataPlotter(pub, 0, False, False)
+    dataPlotter(deaths, 0, False, False)
+    dataPlotter(spec, 7, True, True) #per100k/avg
+    dataPlotter(spec, 7, True, False) #per100k
+    dataPlotter(spec, 7, False, True) #all cases avg
+    dataPlotter(spec, 7, False, False) #all cases
