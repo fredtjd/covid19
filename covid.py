6b90a7e1cbc3a9e2896af413adfa1908be5ac07e
Fred <6234265-fredtjd@users.noreply.gitlab.com>
2021-01-26 16:26:00 +0000

    Add hospital data

--- covid.py
+++ covid.py
@@ -2,6 +2,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 from matplotlib.dates import MonthLocator, DateFormatter
+from matplotlib.ticker import FormatStrFormatter
 import os, sys, time
 import urllib.request as req
 
@@ -15,6 +16,8 @@ Scotland = ['Scotland', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=na
 Wales = ['Wales', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=W92000004&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&metric=newDeathsByPublishDate&format=csv', 32.0]
 NI = ['NI', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=N92000002&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&metric=newDeathsByPublishDate&format=csv', 18.97]
 Oxon = ['Oxfordshire', 'https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&areaCode=E10000025&metric=newCasesByPublishDate&metric=newCasesBySpecimenDate&metric=newDeathsByPublishDate&format=csv', 6.91] #http://insight.oxfordshire.gov.uk/cms/population
+admissions = 'https://api.coronavirus.data.gov.uk/v2/data?areaType=overview&metric=newAdmissions&metric=covidOccupiedMVBeds&metric=hospitalCases&format=csv'
+
 
 pub = ['newCasesByPublishDate', 'New Cases by Publish Date']
 spec = ['newCasesBySpecimenDate', 'New Cases by Specimen Date']
@@ -27,6 +30,7 @@ def csvDownloader():
     if not os.path.isfile(os.path.join(csv_dir, London[0] + '.csv')):
         for loc in locations:
             req.urlretrieve(loc[1], os.path.join(csv_dir, loc[0] + '.csv'))
+        req.urlretrieve(admissions, os.path.join(csv_dir, 'Admissions.csv'))
     else:
         da = str(datetime.today().date())
         print('Checking downloaded csv.')
@@ -42,9 +46,12 @@ def csvDownloader():
                 print('Downloading from server.')
                 for loc in locations:
                     req.urlretrieve(loc[1], os.path.join(csv_dir, loc[0] + '.csv'))
+                req.urlretrieve(admissions, os.path.join(csv_dir, 'Admissions.csv'))
             else:
-                print('No new updates. Exiting.')
-                sys.exit()
+                print('No new updates. Recreating graphs.')
+                #sys.exit()
+        else:
+            print('No new updates. Recreating graphs.')
 
 def dataFrame():
     df = {}
@@ -58,6 +65,18 @@ def dataFrame():
             df[loc[0]][plot_type[0] + 'per100k'] = df[loc[0]][plot_type[0]].div(loc[2])
             df[loc[0]][plot_type[0] + 'avg'] = df[loc[0]][plot_type[0]].rolling(window=7).mean()
             df[loc[0]][plot_type[0] + 'per100kavg'] = df[loc[0]][plot_type[0] + 'per100k'].rolling(window=7).mean()
+    return df
+
+def admissionsDF():
+    df = {}
+    csv_file = os.path.join(csv_dir, 'Admissions.csv')
+    df = pd.read_csv(csv_file, index_col=0)
+    df = df.drop(['areaType', 'areaCode', 'areaName'], axis = 1)
+    df.index = pd.to_datetime(df.index)
+    df = df.sort_index()
+    df['newAdmissionsavg'] = df['newAdmissions'].rolling(window=7).mean()
+    df['covidOccupiedMVBedsavg'] = df['covidOccupiedMVBeds'].rolling(window=7).mean()
+    df['hospitalCasesavg'] = df['hospitalCases'].rolling(window=7).mean()
     return df
 
 def dataPlotter(plot_type, look_back, percapita, avg):
@@ -87,14 +106,33 @@ def dataPlotter(plot_type, look_back, percapita, avg):
     
     plt.savefig(os.path.join(python_dir, 'figs', multiplot_details[1] + '.png'))
 
+def admissionsDP(look_back):
+    plt.clf()
+    startDate = '2020-03-01'
+    endDate = pd.to_datetime(datetime.today() - timedelta(days=look_back))
+    df = admissionsDF()
+    df = df[startDate:endDate]
+    plt.plot(df['newAdmissionsavg'], label='New Admissions')
+    plt.plot(df['covidOccupiedMVBedsavg'], label='Patients Ventilated')
+    plt.plot(df['hospitalCasesavg'], label='Hospitalised Patients')
+    plt.title('Hospital Data (7 day average)')
+    #plt.ylim(0)
+    plt.legend()
+    plt.yscale('log')
+    ax = plt.gca()
+    ax.xaxis.set_major_locator(MonthLocator())
+    ax.xaxis.set_major_formatter(DateFormatter('%b'))
+    ax.xaxis.set_minor_locator(MonthLocator(bymonthday=15))
+    ax.yaxis.set_major_formatter(FormatStrFormatter("%g"))
+    plt.savefig(os.path.join(python_dir, 'figs', 'Hospital Data (7 day average).png'))
+
 if __name__ == '__main__':
     csvDownloader()
-    dataPlotter(pub, 0, True, True)
-    dataPlotter(pub, 0, True, False)
-    dataPlotter(pub, 0, False, True)
-    dataPlotter(pub, 0, False, False)
-#    dataPlotter(deaths, 0, False, False)
-    dataPlotter(spec, 7, True, True) #per100k/avg
-    dataPlotter(spec, 7, True, False) #per100k
-    dataPlotter(spec, 7, False, True) #all cases avg
-    dataPlotter(spec, 7, False, False) #all cases
+                       #100k,   avg
+    dataPlotter(pub,  0, True,  True)
+    dataPlotter(pub,  0, True,  False)
+    dataPlotter(pub,  0, False, True)
+    dataPlotter(pub,  0, False, False)
+    dataPlotter(spec, 7, True,  True)
+    dataPlotter(spec, 7, False, True)
+    admissionsDP(2)
